ea26a22a7a9ff022ef37ca7041379a9a0ee1f782
George Viazemsky <code@and.graphics>
2017-03-14 19:10:43 +0300

    Fix tests

--- Tests/SignatureTest.php
+++ Tests/SignatureTest.php
@@ -8,24 +8,24 @@ use CodeAndGraphics\WalletOne\Payment;
 class SignatureTest extends TestCase
 {
 	public function testBuildSignature() {
-		$payment = new Payment(100000000000, '52er5ReD3UZUjU7r?HefrafreGUGagat');
+		$payment = new Payment(119175088534, 'XkZMYW56NzVbNV1aekxGNVxvT3xwVHExZ005');
 		$payment
 			->setAmount(100)
 			->setCurrencyId(643)
-			->setPaymentId('20100-11')
-			->setDescription('Payment for order #20100-11 in code.and.graphics')
-			->setExpiredDate('2018-12-31T23:59:59')
-			->setSuccessUrl('https://code.and.graphics/w1/success')
-			->setFailUrl('https://code.and.graphics/w1/fail')
+			->setPaymentId('12345-001')
+			->setDescription('Payment for order #12345-001 in MYSHOP.com')
+			->setExpiredDate('2019-12-31T23:59:59')
+			->setSuccessUrl('https://myshop.com/w1/success.php')
+			->setFailUrl('https://myshop.com/w1/fail.php')
 			->setCustomParameters([
-				'CustomParam1' => 'Value1',
-				'CustomParam2' => 'Value2',
-				'CustomParam3' => 'Value3'
+				'MyShopParam1' => 'Value1',
+				'MyShopParam2' => 'Value2',
+				'MyShopParam3' => 'Value3'
 			]);
 
 		$signature = $payment->getSignature();
 
 		// Signature calculated from example: https://www.walletone.com/en/merchant/documentation/
-		$this->assertEquals($signature, 'Zgz9vGUol2/Zhqo5bcsptA==');
+		$this->assertEquals($signature, '7qnRb5mi+2viZbYS3wKlhQ==');
 	}
 }
